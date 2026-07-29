c59084192c6664760936f188667ca7d4a73c9d1b
aalsanie <ahmad.alsanie@hotmail.com>
2026-07-29 18:57:31 +0300

    build: upgrade Gradle wrapper to 8.14.5

--- shamash-intellij-plugin/build.gradle.kts
+++ shamash-intellij-plugin/build.gradle.kts
@@ -52,6 +52,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = "242"
+            untilBuild = "261.*"
         }
     }
 
@@ -59,6 +60,8 @@ intellijPlatform {
         ides {
             create(IntelliJPlatformType.IntellijIdea, "2024.2")
             create(IntelliJPlatformType.IntellijIdea, "2025.2")
+            create(IntelliJPlatformType.IntellijIdea, "2025.3")
+            create(IntelliJPlatformType.IntellijIdea, "2026.1")
         }
     }
 }
